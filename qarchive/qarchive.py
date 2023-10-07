2590a56f38921f97f7b46f59b35aa97f8e119470
panxl <panxl@users.noreply.github.com>
2023-10-07 14:50:14 -0500

    Update

--- qarchive/qarchive.py
+++ qarchive/qarchive.py
@@ -1,102 +1,108 @@
-from typing import List, Union
-from collections.abc import Sequence
+from typing import Union, Optional
 from dataclasses import dataclass, field
+from numpy import ndarray
 import tables
 
 
-class QArchive(object):
-    def __init__(self, filename):
+class QArchive:
+    """A class for reading Q-Chem archive files."""
+    def __init__(self, filename) -> None:
         self.file = tables.open_file(filename, mode='r')
 
-        jobs = []
+        jobs : list[Job] = []
         for job in self.file.root.job:
             node = job._f_list_nodes()[0]
             job_class = job_class_mapping[node._v_name]
             jobs.append(job_class(node))
-        jobs = sorted(jobs, key=lambda job: job.sort_index)
-        self.job = Job(jobs)
+        self.jobs = sorted(jobs, key=lambda job: job.sort_index)
 
-    def close(self):
+    def close(self) -> None:
+        """Close the archive file."""
         self.file.close()
 
-    def __enter__(self):
+    def __enter__(self) -> 'QArchive':
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.close()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'QArchive(\'{self.file.filename}\')'
 
 
 @dataclass(frozen=True, order=True)
-class SP(object):
+class SP:
+    """A class for reading single point energy calculations from Q-Chem archive files."""
     _node: tables.Group = field(compare=False)
     sort_index: int = field(init=False, repr=False)
-    energy_function: List[tables.Group] = field(init=False, compare=False)
+    energy_function: list[tables.Group] = field(init=False, compare=False)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         object.__setattr__(self, 'sort_index', int(self._node._v_parent._v_name))
         object.__setattr__(self, 'energy_function', self._node.energy_function._f_list_nodes())
 
     @property
-    def energy(self):
+    def energy(self) -> float:
+        """Return the energy."""
         return self.energy_function[-1].energy[()]
-    
+
     @property
-    def gradient(self):
-        if 'gradient' in self.energy_function[-1]._v_children:
+    def gradient(self) -> Optional[ndarray]:
+        """Return the gradient."""
+        if 'gradient' in self.energy_function[-1]:
             return self.energy_function[-1].gradient[()]
+        return None
 
     @property
-    def hessian(self):
-        if 'hessian' in self.energy_function[-1]._v_children:
+    def hessian(self) -> Optional[ndarray]:
+        """Return the hessian."""
+        if 'hessian' in self.energy_function[-1]:
             return self.energy_function[-1].hessian[()]
+        return None
 
     @property
-    def mo_coefficients(self):
+    def mo_coefficients(self) -> ndarray:
+        """Return the MO coefficients."""
         return self.energy_function[-1].method.scf.molecular_orbitals.mo_coefficients[()]
 
 
 @dataclass(frozen=True, order=True)
-class GeomOpt(object):
+class GeomOpt:
+    """A class for reading geometry optimization calculations from Q-Chem archive files."""
     _node: tables.Group = field(compare=False)
     sort_index: int = field(init=False, repr=False)
-    iter: List[SP] = field(init=False, compare=False)
+    iter: list[SP] = field(init=False, compare=False)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         object.__setattr__(self, 'sort_index', int(self._node._v_parent._v_name))
-        object.__setattr__(self, 'iter', sorted([SP(iter.sp) for iter in self._node.iter._f_iter_nodes()]))
+        iters = sorted([SP(iter.sp) for iter in self._node.iter._f_iter_nodes()])
+        object.__setattr__(self, 'iter', iters)
 
     @property
-    def energy(self):
+    def energy(self) -> float:
+        """Return the energy of the last iteration."""
         return self.iter[-1].energy
 
     @property
-    def gradient(self):
+    def gradient(self) -> Optional[ndarray]:
+        """Return the gradient of the last iteration."""
         return self.iter[-1].gradient
 
     @property
-    def hessian(self):
+    def hessian(self) -> Optional[ndarray]:
+        """Return the hessian of the last iteration."""
         return self.iter[-1].hessian
 
     @property
-    def mo_coefficients(self):
+    def mo_coefficients(self) -> ndarray:
+        """Return the MO coefficients of the last iteration."""
         return self.iter[-1].mo_coefficients
 
 
-job_class_mapping = {
+Job = Union[SP, GeomOpt]
+
+
+job_class_mapping : dict[str, type] = {
     "sp": SP,
     "geom_opt": GeomOpt,
     }
-
-
-@dataclass(frozen=True)
-class Job(Sequence):
-    _jobs: List[Union[SP, GeomOpt]]
-
-    def __len__(self):
-        return len(self._jobs)
-
-    def __getitem__(self, index):
-        return self._jobs[index]
